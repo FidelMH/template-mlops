c81cdc6e30b04b7434f4cf11b91fcab318f9ca66
FidelMH <moussahaziri.fidel@gmail.com>
2026-03-03 12:03:21 +0400

    fix: add tests, correct docs workflow and Sphinx config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,7 +33,6 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_logo = "_static/img/logo.png"
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 html_title = "Documentation - bases devops - UV"
