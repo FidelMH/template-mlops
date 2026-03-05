44a9481a3f933a910ca4bf3ae1b98ec8dbe7637b
FidelMH <moussahaziri.fidel@gmail.com>
2026-03-05 22:21:19 +0400

    style: Clean up whitespace and formatting in schemas and crud modules

--- app_api/modules/crud.py
+++ app_api/modules/crud.py
@@ -1,13 +1,13 @@
-from sqlalchemy.orm import Session
 from models.models import User
 from sqlalchemy import select
+from sqlalchemy.orm import Session
+
 
 def get_users(session: Session):
     """Retrieve all users from the database."""
-    
     stmt = select(User)
     users = session.scalars(stmt).all()
     return users
 
+
 # def create_user(session: Session):
-    
