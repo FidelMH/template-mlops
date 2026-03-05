8107ed63648ec3677ce950e4317c195e01436cbf
FidelMH <moussahaziri.fidel@gmail.com>
2026-03-05 17:23:35 +0400

    refactor: Adjust import order and clean up whitespace in database connection files

--- app_api/modules/connect.py
+++ app_api/modules/connect.py
@@ -2,20 +2,17 @@ import os
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from models.models import  Base
 
 # Création de l'unité de contrôle
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./local.db")
 engine = create_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(bind=engine)
 
+
 def get_db():
     """Get a database session."""
-    
     db = SessionLocal(bind=engine)()
     try:
         yield db
     finally:
         db.close()
-        
-
