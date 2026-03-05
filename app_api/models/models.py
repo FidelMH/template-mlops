759683aae909a8271f52af05f8630fae22b763cd
FidelMH <moussahaziri.fidel@gmail.com>
2026-03-05 15:59:51 +0400

    feat:Added User Model

--- app_api/models/models.py
+++ app_api/models/models.py
@@ -1,7 +1,16 @@
 from sqlalchemy.orm import DeclarativeBase
-
+from sqlalchemy import ForeignKey, Float, DateTime
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 class Base(DeclarativeBase):
     """Base class for all SQLAlchemy models."""
 
     pass
+
+class User(Base):
+    __tablename__ = "user"
+    id: Mapped[int] =  mapped_column(primary_key=True)
+    name : Mapped[str] = mapped_column()
+    age : Mapped[int]  = mapped_column()
+    score : Mapped[str] = mapped_column()
+    
