fdb841d3120ccf6555b859bce88eab01d5be5785
FidelMH <moussahaziri.fidel@gmail.com>
2026-03-05 22:29:18 +0400

    feat: Implement create_user function to add new users to the database

--- app_api/modules/crud.py
+++ app_api/modules/crud.py
@@ -1,4 +1,5 @@
 from models.models import User
+from models.schemas import UserCreate
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 
@@ -10,4 +11,10 @@ def get_users(session: Session):
     return users
 
 
-# def create_user(session: Session):
+def create_user(user: UserCreate, session: Session) -> User:
+    """Create a new user in the database and return the created user object."""
+    new_user = User(**user.model_dump())
+    session.add(new_user)
+    session.commit()
+    session.refresh(new_user)
+    return new_user
